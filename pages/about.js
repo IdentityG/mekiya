dfcd3fd982b63e0dac738c06b1843ba2b6b32a02
IdentityG <babayesara1@gmail.com>
2024-07-20 00:18:36 -0700

    width image fix

--- pages/about.js
+++ pages/about.js
@@ -26,9 +26,9 @@ const about = () => {
         </div>
       </div>
     </div>
-    <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
-      <img className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]" src="/mekiya.webp" alt="" />
-    </div>
+    <div className="p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
+    <img className="w-full max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-full lg:w-[48rem]" src="/mekiya.webp" alt="" />
+</div>
     <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
       <div className="lg:pr-4">
         <div className="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
